abc7edfdfc50a003971e46c352825b66a1ee568d
alma-98 <alma.budsteddy88@gmail.com>
2026-08-06 17:18:48 +0700

    feat(rocket): create RocketList page skeleton

---
+++ src/pages/RocketList.tsx
@@ -0,0 +1,32 @@
+import "./RocketList.css";
+
+export default function RocketList() {
+  return (
+    <section className="rocket-list-page">
+      <div className="rocket-list-container">
+
+        <header className="rocket-list-header">
+          <h1>SpaceX Rocket Explorer</h1>
+
+          <p>
+            Explore all SpaceX rockets and view detailed
+            information.
+          </p>
+        </header>
+
+        <main className="rocket-list-content">
+
+          {/* SearchBar */}
+
+          {/* Loading */}
+
+          {/* Error */}
+
+          {/* RocketGrid */}
+
+        </main>
+
+      </div>
+    </section>
+  );
+}
