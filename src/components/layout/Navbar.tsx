f1ce3a5474081b114bad30f433c19807f119c954
alma-98 <alma.budsteddy88@gmail.com>
2026-08-06 17:01:28 +0700

    feat(layout): add responsive navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,12 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <header className="navbar">
-      <div className="container navbar-content">
-        <Link to="/" className="logo">
+      <div className="navbar-container">
+
+        <Link to="/" className="navbar-logo">
           🚀 SpaceX Rocket Explorer
         </Link>
+
+        <nav className="navbar-menu">
+
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "active" : ""
+            }
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/rockets"
+            className={({ isActive }) =>
+              isActive ? "active" : ""
+            }
+          >
+            Rockets
+          </NavLink>
+
+        </nav>
+
       </div>
     </header>
   );
